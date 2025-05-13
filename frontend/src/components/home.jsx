f260360044bd859272b19981705439358c1a477b
bennyd6 <228r1a66d6@cmrec.ac.in>
2025-05-13 14:03:37 +0530

    made some animation stuff

--- frontend/src/components/home.jsx
+++ frontend/src/components/home.jsx
@@ -4,27 +4,24 @@ import TypewriterHero from './typewriter'
 import web from '../assets/webcom.png'
 import Fade from 'react-awesome-reveal';
 
-
-export default function Home(){
-
-        
-    return(
-        <>    
-        <div className="h-main">
-            <Navbar />
-            <div className="h-in">   
-                <div className="h-in-1">
-                    <Fade direction="up">
-                        <TypewriterHero />
-                    </Fade>
-                </div>
-                <div className="h-in-2">
-                    {/* <Fade direction="right"> */}
+export default function Home() {
+    return (
+        <>
+            <div className="h-main">
+                <Navbar />
+                <div className="h-in">
+                    <div className="h-in-1">
+                        <Fade direction="up">
+                            <TypewriterHero />
+                        </Fade>
+                    </div>
+                    <div className="h-in-2">
+                        {/* <Fade direction="right"> */}
                         <img src={web} alt="" />
-                    {/* </Fade> */}
+                        {/* </Fade> */}
+                    </div>
                 </div>
             </div>
-        </div>
         </>
     )
 }
